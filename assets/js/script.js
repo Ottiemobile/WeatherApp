ab1c2fb6e66656bf7f088ba7e5082f6b5ca8dc00
Ethan Torres <torresethan99@gmail.com>
2020-06-28 21:31:07 -0500

    web app build 0.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,9 @@
-const api = {
-    key: "ecddcae668690a57c873375393f18153",
-baseurl: "https://api.openweathermap.org/data/2.5/"
-}
+var date = "";
+var weather = "";
 
-const searchbar = document.querySelector(".search-bar");
-searchbar.addEventListener("keypress",setQuery);
 
-function setQuery(evt) {
-    if (evt.keyCode == 13){
-        getResults(searchbar.value);
-        console.log(searchbar.value);
-    }
-}
+//function of what happens when a user types information and hits the search button
+
+//
+
+//function that pulls the 
